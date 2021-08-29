9862786a9e372a5837f6afacd41dc55eaf621dc5
7nik <kifiranet@gmail.com>
2021-08-30 00:51:24 +0300

    prevent multistart

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,26 @@
 import App from './App.svelte';
 
-const a = document.createElement("a");
-a.innerText = "Import tags";
-a.href = "#";
-a.addEventListener("click", (ev) => {
-    ev.preventDefault();
-    document.getElementById("mobile_menu_icon")?.click();
-    startApp();
-});
-
-const li = document.createElement("li");
-li.append(a);
-
-const ul = document.createElement("ul");
-ul.style.marginTop = "20px";
-ul.append(li);
-
-document.getElementById("mobile_menu")?.append(ul);
-
 if (window.location.hash) {
     startApp();
+} else {
+    const ul = document.createElement("ul");
+    ul.style.marginTop = "20px";
+
+    const li = document.createElement("li");
+
+    const a = document.createElement("a");
+    a.innerText = "Import tags";
+    a.href = "#";
+    a.addEventListener("click", (ev) => {
+        ev.preventDefault();
+        document.getElementById("mobile_menu_icon")?.click();
+        startApp();
+        ul.remove();
+    });
+    
+    li.append(a);
+    ul.append(li);
+    document.getElementById("mobile_menu")?.append(ul);
 }
 
 function startApp () {
