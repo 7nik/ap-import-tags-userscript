effb46e422768abe73105920ad7ce82885261bc9
7nik <kifiranet@gmail.com>
2024-09-25 14:56:46 +0300

    feat: make storage deeply reactive

--- src/libs/storage.svelte.ts
+++ src/libs/storage.svelte.ts
@@ -1,5 +1,5 @@
 import type { SavedResult } from "./matcher.svelte";
-import { untrack } from "svelte";
+import { tick, untrack } from "svelte";
 
 const PREFIX = "AP_tag_importer_";
 
@@ -18,16 +18,36 @@ type LocalData = {
 };
 
 const cache: Partial<LocalData> = $state({});
+const subCount: Record<string, number> = {};
+const destroyer: Record<string, null | (() => void)> = {};
 
 const storageMethods = {
     get<N extends keyof LocalData>(name: N): LocalData[N] | null {
-        if (!(name in cache) && PREFIX.concat(name) in window.localStorage) {
-            untrack(() => {
+        untrack(() => {
+            if (!(name in cache) && PREFIX.concat(name) in window.localStorage) {
                 cache[name] = JSON.parse(window.localStorage[PREFIX.concat(name)]);
-                // if (cache[name] && typeof cache[name] === "object") {
-                //     let obj: any = $state(cache[name]);
-                //     $effect(() => { storage.set(name, obj); })
-                // }
+            }
+        });
+        if (cache[name] && typeof cache[name] === "object" && $effect.tracking()) {
+            // StartStopNotifier to automatically save the object
+            $effect(() => {
+                if (!subCount[name]) {
+                    destroyer[name] = $effect.root(() => {
+                        $effect(() => {
+                            storageMethods.set(name, cache[name]!);
+                        });
+                    });
+                }
+                subCount[name] = (subCount[name] ?? 0) + 1;
+                return () => {
+                    tick().then(() => {
+                        subCount[name] -= 1;
+                        if (subCount[name] === 0) {
+                            destroyer[name]?.();
+                            destroyer[name] = null;
+                        }
+                    });
+                };
             });
         }
         return cache[name] ?? null;
